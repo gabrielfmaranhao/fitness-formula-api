0e7601e37510f4c3bae2b8d40d4f9cbe47d548bb
Gabriel Maranhao <gabrielmaranhao48@gmail.com>
2023-01-04 03:29:15 -0300

    feat settings app

--- __core__/settings.py
+++ __core__/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'workout_exercise'
+    'workout_exercise',
 ]
 
 MIDDLEWARE = [

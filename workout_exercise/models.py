c0e5ea1d44383908366b877740c670eee1514db7
Gabriel Maranhao <gabrielmaranhao48@gmail.com>
2023-01-04 03:14:04 -0300

    feat models workout_exercise and exercise

--- workout_exercise/models.py
+++ workout_exercise/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Workout_exercise(models.Model):
+    id = models.UUIDField()
+    equipment = models.CharField(null=True)
+    description = models.CharField(null=True)
+    sets = models.PositiveIntegerField()
+    reps = models.PositiveIntegerField()
+    workout = models.ForeignKey('workout.Workout', on_delete=models.CASCADE, related_name="workout_exercises")
+    exercise = models.ForeignKey('workout_exercise.Exercise', on_delete=models.CASCADE, related_name="workout_exercises")
+
+class Exercise(models.Model):
+    id = models.UUIDField()
+    title = models.CharField()

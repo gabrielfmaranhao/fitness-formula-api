2921e39f594c9f39cb84167162e0bc12d6f92c43
Gabriel Maranhao <gabrielmaranhao48@gmail.com>
2023-01-04 02:57:38 -0300

    feat/model workout

--- workout/models.py
+++ workout/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class MuscleChoices(models.TextChoices):
+    CHOICES_LEGS = "legs"
+    CHOICES_ARMS = "arms"
+    CHOICES_BACK = "back"
+    CHOICES_CHEST = "chest"
+    CHOICES_SHOLDERS = "sholders"
+    CHOICES_CARDIO = "cardio"
+class DayChoices(models.TextChoices):
+    CHOICES_MONDAY = "monday"
+    CHOICES_TUESDAY = "tuesday"
+    CHOICES_WEDNESDAY = "wednesday"
+    CHOICES_THURSDAY = "thursday"
+    CHOICES_FRIDAY = "friday"
+    CHOICES_SATURDAY = "saturday"
+    CHOICES_SUNDAY = "sunday"
+class Workout(models.Model):
+    id = models.UUIDField(primary_key=True)
+    muscle_group = models.CharField(choices=MuscleChoices.choices)
+    day = models.CharField(choices=DayChoices.choices)
+    title = models.CharField(null=True)
+    description = models.CharField(null=True)

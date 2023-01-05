f52f48cfff765bec7eeb5d6775ed2a649e3e2a46
douglas <douglas.estefanio@gmail.com>
2023-01-05 15:04:21 -0300

    feat: update rating

--- rating/models.py
+++ rating/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 import uuid
 
-
+class RatingChoices(models.IntegerChoices):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
 class Rating(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     comment = models.CharField(max_length=200)
-    stars = models.PositiveSmallIntegerField()
+    stars = models.PositiveSmallIntegerField(choices=RatingChoices.choices)
 
     student = models.ForeignKey(
         "user.User",

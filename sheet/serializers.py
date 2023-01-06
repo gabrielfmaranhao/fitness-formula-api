54b61fd73bdc612920cb13b3fb8d596bbf408481
Vitor Brasil <vitorlbrasil@hotmail.com>
2023-01-06 17:10:05 -0300

    fix: fix workout routes bugs and unwanted behaviors

--- sheet/serializers.py
+++ sheet/serializers.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 from rest_framework import serializers
+from user.serializers import UserSerializer, TrainerSerializer
 from .models import Sheet
+import ipdb
 
 
 class SheetSerializer(serializers.ModelSerializer):
-    # duration = serializers.SerializerMethodField()
+    student_name = serializers.SerializerMethodField()
+    trainer_name = serializers.SerializerMethodField()
+    duration = serializers.SerializerMethodField()
 
-    # def get_duration(self, obj: Sheet):
-    #     start_date = datetime.strptime(obj.created_at, "%Y/%m/%d")
-    #     end_date = datetime.strptime(obj.until, "%Y/%m/%d")
-    #     delta = end_date - start_date
+    def get_student_name(self, obj: Sheet):
+        return f"{obj.student.first_name} {obj.student.last_name}"
 
-    #     return f"{delta.days} days"
+    def get_trainer_name(self, obj: Sheet):
+        return f"{obj.trainer.first_name} {obj.trainer.last_name}"
+
+    def get_duration(self, obj: Sheet):
+        delta = obj.until - obj.created_at
+        return f"{delta.days} days"
 
     class Meta:
         model = Sheet
@@ -20,9 +27,11 @@ class SheetSerializer(serializers.ModelSerializer):
             "type",
             "created_at",
             "until",
-            # "duration",
+            "duration",
             "trainer",
+            "trainer_name",
             "student",
+            "student_name",
         ]
         read_only_fields = [
             "trainer",
